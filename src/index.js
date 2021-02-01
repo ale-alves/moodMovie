d614058ed8a6afd20295bded931cc922e83dc9dd
Juliana <juliana.souzas@yahoo.com.br>
2021-02-01 13:10:08 -0300

    spa

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import home from "./pages/home/index.js"
+import sentimentos from "./pages/sentimentos/index.js"
+import filmes from "./pages/filmes/index.js"
+
+
+const main = document.querySelector("#root");
+
+const init = () => {
+window.addEventListener("hashchange", () => {
+    main.innerHTML = ""
+    switch (window.location.hash){
+        case " ":
+            main.appendChild(home());
+            break;
+            case "#sentimentos":
+                main.appendChild(sentimentos());
+            break;
+            case "#filmes":
+                main.appendChild(filmes());
+            break;
+            default:
+                    main.appendChild(home());
+    }
+
+
+
+})
+
+}
+
+window.addEventListener("load", () => {
+
+    main.appendChild(home());
+    init()
+
+})
