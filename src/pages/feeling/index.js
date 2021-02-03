6b6a6e2b802ca28477ee00fa9f25760380e4a5fd
Alessandra Alves <a.ale_alves@hotmail.com>
2021-02-03 02:06:51 -0300

    atualização

--- src/pages/feeling/index.js
+++ src/pages/feeling/index.js
@@ -2,20 +2,13 @@ export default () => {
   const container = document.createElement('div');
 
   const template = `
-    <header>
-        <nav class="navbar">
-            <ul class="navbar-list">
-                <li class="list">
-                    <a class="title" href="./#home"><img class="logo-github" src="img/next.png" alt="logo github" /></a>
-                </li>
-        </nav>
-    </header>
+    
     <main>
-        <h2 class="title">Oi tudo bem? Como está seu humor hoje?</h2>
+        <h2 class="title-feeling">Oi tudo bem? Como está seu humor hoje?</h2>
         <section>   
             <a href="./#movie?genre=animation,comedy"> <button id="btn-stressed" class="btn-stressed">Estressade</button></a>
             <a href="./#movie?genre=fiction,thriller"> <button id="btn-inspired" class="btn-inspired" >Inspirade</button></a>
-            <a href="./#movie?genre=come const stressedButton = container.querySelector('#btn-stressed');dy,drama"> <button id="btn-happy" class="btn-happy" >Feliz</button></a>
+            <a href="./#movie?genre=comedy,drama"> <button id="btn-happy" class="btn-happy" >Feliz</button></a>
             <a href="./#movie?genre=fiction,comedy"> <button id="btn-bored" class="btn-bored" >Entediade</button></a>
             <a href="./#movie?genre=drama,comedy"> <button id="btn-heart-broken" class="btn-heartbroken" >Coração Partido</button></a>
             <a href="./#movie?genre=animation,comedy"> <button id="btn-sad" class="btn-sad" >Triste</button></a>
--- src/pages/movie/index.js
+++ src/pages/movie/index.js
@@ -7,12 +7,7 @@ export default async () => {
     
     container.innerHTML = `
         <header>
-            <nav class="navbar">
-                <ul class="navbar-list">
-                    <li class="list">
-                        <a class="title" href="./#home"><img class="logo-github" src="img/next.png" alt="logo github"></a>
-                    </li>
-            <div>
+           <div>
                 <h2 class="title-movies">Achamos que vai gostar dessa seleção que fizemos para você <img class="logo-github"    src="img/favourite.png" alt="logo github"></h2>
             </div>
         </header>
