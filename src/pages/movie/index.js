690b762e1a712fcc98e80aec37cd5e059c85b3fd
caroline costa <carolmarcionilia@gmail.com>
2021-02-03 02:10:24 -0300

    add function to call api

--- src/pages/movie/index.js
+++ src/pages/movie/index.js
@@ -1,22 +1,20 @@
 export default () => {
   const container = document.createElement('div');
-
-  const template = `
-
-  <header>
-  <nav class="navbar">
-      <ul class="navbar-list">
-        <li class="list">
-          <a class="title" href="./#home"><img class="logo-github" src="img/next.png" alt="logo github" /></a>
-        </li>
-  </header>
-  <h2 class="title-movies">Achamos que vai gostar dessa seleção que fizemos para você <img class="logo-github" src="img/favourite.png" alt="logo github" /> </h2>
-  <div >
-  <img class="img"  src="./img/capa-soul.jpg" alt="capa de filme" width="150px" height="180px">
-  </div>  
+  const movieHeader = document.createElement('header')
+  
+  movieHeader.innerHTML= `
+    <nav class="navbar">
+        <ul class="navbar-list">
+          <li class="list">
+            <a class="title" href="./#home"><img class="logo-github" src="img/next.png" alt="logo github"></a>
+          </li>
+        <div>
+        <h2 class="title-movies">Achamos que vai gostar dessa seleção que fizemos para você <img class="logo-github" src="img/favourite.png" alt="logo github"></h2>
+        </div>
   `;
 
-  container.innerHTML = template;
+  container.appendChild(movieHeader);
+
 
   return container
 }
